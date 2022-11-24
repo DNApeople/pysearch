00733cabec789591b05d7f51101157866914459b
DNApeople <112553123+DNApeople@users.noreply.github.com>
2022-11-24 16:44:40 +0530

    Add files via upload

---
+++ search.py
@@ -0,0 +1,34 @@
+import os
+
+global path_with_file
+
+# the location where you want to save the file (change as you pleas) eg- "H:\FoldeName" + "FileName.txt"
+path_with_file = "F:\pysearch/"+ 'urls.txt'
+
+def make_a_file():
+    ALL_urls  = input('paste all urls:\t')
+    List_urls = ALL_urls.split(' ')
+    
+    with open(path_with_file ,'w+') as url_file:
+        for items in List_urls:
+            url_file.write('%s\n' %items)
+            print("saved url:")
+        url_file.close()
+
+make_a_file()
+
+def search():
+    with open(path_with_file) as search_urls:
+        for line in search_urls:
+            start = 'start ' + line
+            os.system(start)
+
+
+
+ask_for_search = input('\nDo you require immediat search ? [y,n]\t')
+ask_ = ask_for_search.lower()
+if ask_ == 'y':
+    search()
+else:
+    exit()
+        
